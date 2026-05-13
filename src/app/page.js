f5c83042380d27d8e86341efc2b686e96a648c5b
Anastasia Solodova <132252079+nastiasolo@users.noreply.github.com>
2026-05-13 19:51:40 +0200

    removed imports not in use

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,8 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
+
 import React from "react";
-import ReactDOM from "react-dom/client";
 
 import HomePage from "../pages/HomePage/HomePage.jsx";
-import DestinationPage from "../pages/DestinationPage/DestinationPage.jsx";
-import AboutUsPage from "../pages/AboutUsPage/AboutUsPage.jsx";
-import NasaCollaboration from "../pages/NasaCollaborationPage/NasaCollaborationPage.jsx";
-
-import { WishlistProvider } from "../context/WishlistContext.jsx";
-
 export default function Home() {
   return <HomePage />;
 }
